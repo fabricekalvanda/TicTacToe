a831db52b7792c80eaa0647e2cb41df6bea6838e
fabricekalvanda <fabricekalvanda@yahoo.com>
2023-05-15 16:05:37 -0700

    Unit test failling with incorrect initial model

--- TicTacToe/Models/TicModel.swift
+++ TicTacToe/Models/TicModel.swift
@@ -15,15 +15,42 @@ enum cell: String {
 
 struct TicModel{
     private var _grid: [Cell]
+    private var _winner: Winner
     
     init(){
         _grid = []
         for _ in 0..<9 {
             _grid.append(Cell.b)
         }
+        _winner = .none
     }
     
     var grid: [Cell]{
         get { _grid }
+    }
+    
+    var winner: Winner {
+        get { _winner }
+    }
+    
+    var isGridFull: Bool {
+        get { grid.filter {$0 == Cell.b }.count == 0 }
+    }
+    
+    mutating func setCell(n:Int, c: Cell) {
+        guard _grid.indices.contains(n) else {
+            return
+        }
+        guard _grid[n] == .b else {
+            return
+        }
+        _grid[n] = c
+    }
+    
+    mutating func updateGameStatus() -> Bool {
+        let winOptions: [Set<Int>] = [
+            [0,1,2], [3,4,5], [6,7,8],
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,4,8], [2,4,6]]
     }
 }
--- TicTacToeTests/TicTacToeTests.swift
+++ TicTacToeTests/TicTacToeTests.swift
@@ -21,7 +21,7 @@ final class TicViewModelTests: XCTestCase {
         
         XCTAssertEqual((ticViewModel.grid.filter{ $0 == Cell.b }.count), 9)
     }
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
