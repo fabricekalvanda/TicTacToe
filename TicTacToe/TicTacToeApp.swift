6734f4075727e923c89a4aaf04e6d09803742224
fabricekalvanda <fabricekalvanda@yahoo.com>
2023-05-15 14:10:56 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Student Account on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
