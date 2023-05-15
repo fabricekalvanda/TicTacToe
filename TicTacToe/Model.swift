867a97e18281a118cd9fc70ffa70675b1e63e4f6
fabricekalvanda <fabricekalvanda@yahoo.com>
2023-05-15 14:42:21 -0700

    Green - adds grid code to model to check 9 cells

--- TicTacToe/Model.swift
+++ TicTacToe/Model.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
+enum cell: String {
+    case x = "X"
+    case o = "O"
+    case b = "" // blank
+}
+
 struct TicModel{
+    private var _grid: [cell]
     
+    init(){
+        _grid = []
+        for _ in 0..<9 {
+            _grid.append(cell.x)
+        }
+    }
 }
